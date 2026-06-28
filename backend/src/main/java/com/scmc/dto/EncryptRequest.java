df626dc5e5176b50c898665caba53465085bb890
Angel-Suescun <asuescunr@unal.edu.co>
2026-06-27 22:09:52 -0500

    feat: update encrypt request and response DTOs

--- backend/src/main/java/com/scmc/dto/EncryptRequest.java
+++ backend/src/main/java/com/scmc/dto/EncryptRequest.java
@@ -1,5 +1,48 @@
 package com.scmc.dto;
 
-public class EncryptRequest {
+import java.util.List;
 
+public class EncryptRequest {
+  private String message;
+
+  private List<Integer> permutation;
+
+  private Integer blockSize;
+
+  private Integer shift;
+
+  public EncryptRequest () {
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public List<Integer> getPermutation() {
+    return permutation;
+  }
+
+  public void setPermutation(List<Integer> permutation) {
+    this.permutation = permutation;
+  }
+
+  public Integer getShift() {
+    return shift;
+  }
+
+  public void setShift(Integer shift) {
+    this.shift = shift;
+  }
+
+  public Integer getBlockSize() {
+    return blockSize;
+  }
+
+  public void setBlockSize(Integer blockSize) {
+    this.blockSize = blockSize;
+  }
 }
--- backend/src/main/java/com/scmc/dto/EncryptResponse.java
+++ backend/src/main/java/com/scmc/dto/EncryptResponse.java
@@ -1,5 +1,16 @@
 package com.scmc.dto;
 
+import java.util.List;
+
 public class EncryptResponse {
 
+  private String originalMessage;
+
+  private String paddedMessage;
+
+  private String permutedMessage;
+
+  private String encryptedMessage;
+
+  private List<AuditStep> audit;
 }
