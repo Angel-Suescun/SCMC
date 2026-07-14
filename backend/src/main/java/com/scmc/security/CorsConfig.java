e30829fefb8154d7f214cf3e071442a0f885d46e
Angel-Suescun <asuescunr@unal.edu.co>
2026-07-14 17:18:18 -0500

    feat: cors

--- backend/src/main/java/com/scmc/security/CorsConfig.java
+++ backend/src/main/java/com/scmc/security/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
     CorsConfiguration configuration = new CorsConfiguration();
 
     configuration.setAllowedOrigins(List.of(
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://scmc-blush.vercel.app"
     ));
 
     configuration.setAllowedMethods(List.of(
