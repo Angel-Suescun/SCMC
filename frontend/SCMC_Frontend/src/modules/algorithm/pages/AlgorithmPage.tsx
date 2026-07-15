2fe8423906f905bafed25c1bd10f00387ac08061
Angel-Suescun <asuescunr@unal.edu.co>
2026-07-14 21:18:33 -0500

    feat: update

--- frontend/SCMC_Frontend/src/modules/algorithm/pages/AlgorithmPage.tsx
+++ frontend/SCMC_Frontend/src/modules/algorithm/pages/AlgorithmPage.tsx
@@ -20,9 +20,9 @@ export default function ImplementationPage() {
 
             <PermutationImplementationSection />
 
-            <ModularImplementationSection />
-
             <ShiftNormalizationSection />
+
+            <ModularImplementationSection />
 
             <EncryptServiceSection />
 
